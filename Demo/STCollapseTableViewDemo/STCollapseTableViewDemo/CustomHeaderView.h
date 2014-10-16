f640141a0a75981606bc2dc5efcd00ac5a0305f4
Fredrik Göransson <fredrik.goransson@gmail.com>
2014-10-16 12:21:17 +0200

    Demo code for reusable header/footers

---
+++ Demo/STCollapseTableViewDemo/STCollapseTableViewDemo/CustomHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  Demo2HeaderView.h
+//  STCollapseTableViewDemo
+//
+//  Created by Fredrik Göransson on 16/10/14.
+//  Copyright (c) 2014 iSofTom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomHeaderView : UITableViewHeaderFooterView
+
+@end
